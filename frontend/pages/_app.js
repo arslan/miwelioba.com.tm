70e8991d047991e5df7098e726b7bc40ccb9d414
Arslan A <arslan@hillitilsimat.com>
2022-10-10 11:03:42 +0500

    add: created content structure for backend

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import ErrorPage from 'next/error'
+
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // Get the data we need on all pages
+  const { global } = pageProps;
+  console.log(pageProps)
+  if (global == null) {
+    return <ErrorPage statusCode={404} />
+  }
+
+  const { metadata, favicon, metaTitleSuffix } = global.attributes
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
