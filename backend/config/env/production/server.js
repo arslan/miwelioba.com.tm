2298bd900ca92d6335dde4b224c271d15f64ee80
Arslan Atajanov <arslan@hillitilsimat.com>
2022-11-14 00:54:48 +0500

    fix prod host server

--- backend/config/env/production/server.js
+++ backend/config/env/production/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: env('HOST', 'miwelioba.hillitilsim.at/strapi'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
