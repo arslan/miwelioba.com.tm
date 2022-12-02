2ad820592822c4919fb68cce68d00b07d47c7930
Arslan Atajanov <arslan@hillitilsimat.com>
2022-12-02 11:31:52 +0500

    fix wheat icon alignment

--- frontend/components/sections/feature-columns.js
+++ frontend/components/sections/feature-columns.js
@@ -6,7 +6,7 @@ export default function FeatureColumns({ data }) {
   return (
     <div className="container flex flex-row flex-wrap items-center justify-between gap-8 mt-12 mb-20 text-white lg:mt-24 lg:mb-36">
       {columnFeatures.map(({ id, title, media }) => (
-        <div key={id} className="mx-auto text-center w-36">
+        <div key={id} className="h-24 mx-auto text-center w-36">
           <div className="mx-auto mb-2 w-14">
             <NextImage media={media} />
           </div>
